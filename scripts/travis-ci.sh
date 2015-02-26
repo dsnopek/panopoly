1d20c6904508940abed91cdbf061080e7b4cdceb
David Snopek <dsnopek@gmail.com>
2015-02-26 21:04:02 +0000

    Attempt to setup apache!

--- scripts/travis-ci.sh
+++ scripts/travis-ci.sh
@@ -96,12 +96,25 @@ system_install() {
   header Downloading Selenium
   wget http://selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar
  
-  # Disable sendmail
+  # Setup Apache.
+  sudo apt-get install apache2 libapache2-mod-fastcgi
+  sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+  sudo a2enmod rewrite actions fastcgi alias
+  echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+  ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
+  sudo cp -f drupal/profiles/panopoly/travis-ci-apache /etc/apache2/sites-available/default
+  sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
+  sudo service apache2 restart
+
+  # Disable sendmail (do in both local PHP for install, and FPM for when
+  # serving the actual site).
   echo sendmail_path=`which true` >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+  echo sendmail_path=`which true` >> ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.ini
 
   # Enable APC
-  echo "extension=apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-  echo "apc.shm_size=256M" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+  echo "extension=apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.ini
+  echo "apc.shm_size=256M" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.ini
+
 }
 
 # before_tests
