ea7d7d35429834d57e1a7f9dad8eb9649f7aa9e4
David Snopek <dsnopek@gmail.com>
2015-02-26 19:01:17 +0000

    Only run the test we care about.

--- scripts/travis-ci.sh
+++ scripts/travis-ci.sh
@@ -186,10 +186,10 @@ run_tests() {
   fi
 
   # First, run all the tests in Firefox.
-  run_test ./bin/behat --config behat.travis.yml
+  run_test ./bin/behat --config behat.travis.yml features/search.feature --tags dblog
 
   # Then run some Chrome-only tests.
-  run_test ./bin/behat --config behat.travis.yml -p chrome
+  #run_test ./bin/behat --config behat.travis.yml -p chrome
 }
 
 # after_tests
