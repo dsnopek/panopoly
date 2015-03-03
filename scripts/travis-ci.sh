58d20a93541b6b427fe124984453a0fa88228170
David Snopek <dsnopek@gmail.com>
2015-03-03 20:10:19 +0000

    Try using an external php.ini file.

--- scripts/travis-ci.sh
+++ scripts/travis-ci.sh
@@ -95,18 +95,10 @@ system_install() {
   # Get Selenium
   header Downloading Selenium
   wget http://selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar
+
+  # Add our php.ini to configure PHP.
+  phpenv config-add drupal/profiles/panopoly/travis-ci/php.ini
  
-  # Disable sendmail
-  echo sendmail_path=`which true` >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-
-  # Enable APC
-  echo "extension=apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-  echo "apc.shm_size=256M" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-
-  # Increase the MySQL connection timeout on the PHP end.
-  echo "mysql.connect_timeout=3000" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-  echo "default_socket_timeout=3000" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-
   # Increase the MySQL server timetout and packet size.
   mysql -e "SET GLOBAL wait_timeout = 36000;"
   mysql -e "SET GLOBAL max_allowed_packet = 33554432;"
