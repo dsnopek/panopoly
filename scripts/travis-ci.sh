0de167774ef172b14c465645336892fccaa5c438
David Snopek <dsnopek@gmail.com>
2015-05-26 09:30:21 -0500

    Run all the tests!

--- scripts/travis-ci.sh
+++ scripts/travis-ci.sh
@@ -190,10 +190,10 @@ run_tests() {
   fi
 
   # First, run all the tests in Firefox.
-  run_test ./bin/behat --config behat.travis.yml features/panopoly_magic/livepreview.feature
+  run_test ./bin/behat --config behat.travis.yml
 
   # Then run some Chrome-only tests.
-  #run_test ./bin/behat --config behat.travis.yml -p chrome
+  run_test ./bin/behat --config behat.travis.yml -p chrome
 }
 
 # after_tests
