125205d7548407467b7a407f574d0e40e67023bf
Fabian Franz <github@fabian-franz.de>
2015-08-05 09:56:03 -0700

    Test on legacy infra for real.

--- .drupal-ti/runners/panopoly-behat/script.sh
+++ .drupal-ti/runners/panopoly-behat/script.sh
@@ -16,6 +16,7 @@ panopoly_header Running tests
 #panopoly_header Hacking panopoly_test_panels.behat.inc ...
 #rm -f steps/panopoly_test_panels.behat.inc
 #mv -f $TRAVIS_BUILD_DIR/hacks/panopoly_test_panels.behat.inc steps/
+rm -f $TRAVIS_BUILD_DIR/hacks/panopoly_test_panels.behat.inc
 mv -f $TRAVIS_BUILD_DIR/hacks/behat.travis.yml.dist .
 
 # If this isn't an upgrade, we test if any features are overridden.
